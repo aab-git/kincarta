568bad5e0abcb781b137fc56dddc2d91481eb70a
ruslantt <rtoktosartov@gmail.com>
2020-11-23 12:03:25 -0600

    Steps class was created

---
+++ src/test/java/com/amazon/steps/AlexaSteps.java
@@ -0,0 +1,33 @@
+package com.amazon.steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class AlexaSteps {
+
+    @Given("^the user navigates to \"([^\"]*)\" homepage$")
+    public void the_user_navigates_to_homepage(String arg1) throws Throwable {
+
+    }
+
+    @Given("^the user searches for \"([^\"]*)\"$")
+    public void the_user_searches_for(String arg1) throws Throwable {
+
+    }
+
+    @Given("^the user navigates to the second page$")
+    public void the_user_navigates_to_the_second_page() throws Throwable {
+
+    }
+
+    @Given("^the user selects the third item$")
+    public void the_user_selects_the_third_item() throws Throwable {
+
+    }
+
+    @Then("^assert that the user would be able to add item to the cart$")
+    public void assert_that_the_user_would_be_able_to_add_item_to_the_cart() throws Throwable {
+
+    }
+
+}

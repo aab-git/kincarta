6c291a1e0da975f1963b7b8abcf06b16e4ed8683
ruslantt <rtoktosartov@gmail.com>
2020-11-23 11:34:33 -0600

    FirefoxDriver was created

---
+++ src/test/java/com/amazon/utilitiies/FirefoxDriver.java
@@ -0,0 +1,22 @@
+package com.amazon.utilitiies;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirefoxDriver {
+
+    public static WebDriver loadFirefoxDriver () {
+
+        WebDriverManager.firefoxdriver().setup();
+
+        WebDriver driver = new org.openqa.selenium.firefox.FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        return driver;
+
+    }
+
+}

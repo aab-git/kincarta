1b1e59f8d7ec77176eec85bd10cabbff8ec91153
ruslantt <rtoktosartov@gmail.com>
2020-11-23 11:47:14 -0600

    Driver was setup

--- src/test/java/com/amazon/utilitiies/Driver.java
+++ src/test/java/com/amazon/utilitiies/Driver.java
@@ -9,9 +9,28 @@ public class Driver {
     private static WebDriver driver;
 
     public static WebDriver getDriver () {
-        if ()
+        if (driver == null) {
+            String browser = ConfigsReader.getProperty("browser").toLowerCase();
 
+            if (browser.equals("firefox"))
+                driver = FirefoxDriver.loadFirefoxDriver();
 
+            else
+                driver = ChromeDriver.loadChromeDriver();
+        }
 
+        return driver;
+
+    }
+    public static void closeDriver () {
+        if (driver == null)
+            return;
+        try {
+            driver.close();
+            driver.quit();
+            driver = null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
